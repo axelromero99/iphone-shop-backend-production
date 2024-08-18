7a1bca48aefaef210695dec9f5d97c8b6b84db04
arrecode <axelromero99@outlook.com>
2024-08-18 19:21:21 -0300

    trying to do a clean deploy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  // app.useGlobalInterceptors(new TransformInterceptor());
+  let httpsOptions = {};
 
-  await app.listen(3000);
+  // Only use HTTPS options if in development mode
+  // if (process.env.NODE_ENV === 'development') {
+  //   httpsOptions = {
+  //     key: fs.readFileSync('./src/cert/key.pem'),
+  //     cert: fs.readFileSync('./src/cert/cert.pem'),
+  //   };
+  // }
+
+  // Pass the HTTPS options only if they are set (development)
+  const app = await NestFactory.create(AppModule, Object.keys(httpsOptions).length ? { httpsOptions } : undefined);
+  app.enableCors();
+  const logger = new Logger('Bootstrap');
+
+  // app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen(process.env.PORT || 3000);
+
+  logger.log(`App running on port ${process.env.PORT}`);
 }
+
 bootstrap();
